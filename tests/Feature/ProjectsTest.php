0e9819ff97107bc960c2b289dfa160ec43e89949
Pablo Ledesma <pabloledes83@gmail.com>
2020-04-02 21:58:50 -0500

    scoping projects

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,12 +11,25 @@ class ProjectsTest extends TestCase
     use WithFaker, RefreshDatabase;
 
      /** @test */
-     public function only_authenticated_users_can_create_projects()
+     public function guests_cannot_create_projects()
      {
          $attributes = factory('App\Project')->raw();
 
          $this->post('/projects', $attributes)->assertRedirect('login');
      }
+
+    /** @test */
+    public function guests_cannot_view_projects()
+    {
+        $this->get('/projects')->assertRedirect('login');
+    }
+
+    /** @test */
+    public function guests_cannot_view_a_single_project()
+    {
+        $project = factory('App\Project')->create();
+        $this->get($project->path())->assertRedirect('login');
+    }
 
     /** @test */
     public function a_user_can_create_a_project()
@@ -38,6 +51,30 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
+    public function a_user_can_view_their_project()
+    {
+        $this->be(factory('App\User')->create());
+
+        $this->withoutExceptionHandling();
+
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
+
+        $this->get('/projects/' . $project->id)
+            ->assertSee($project->title)
+            ->assertSee($project->description);
+    }
+
+    /** @test */
+    public function an_authenticated_user_cannot_view_the_projects_of_others()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->be(factory('App\User')->create());
+
+        $this->get($project->path())->assertStatus(403);
+    }
+
+    /** @test */
     public function a_project_requires_a_title()
     {
         $this->actingAs(factory('App\User')->create());
@@ -53,19 +90,6 @@ class ProjectsTest extends TestCase
         $attributes = factory('App\Project')->raw(['description' => '']);
 
         $this->post('/projects', $attributes)->assertSessionHasErrors('description');
-    }
-
-    /** @test */
-    public function a_user_can_view_a_project()
-    {
-        $this->withoutExceptionHandling();
-
-        $this->actingAs(factory('App\User')->create());
-        $project = factory('App\Project')->create();
-
-        $this->get('/projects/' . $project->id)
-            ->assertSee($project->title)
-            ->assertSee($project->description);
     }
 
 }
